35c644a4c74c96f5bd93fbda974f601de487894a
Niuhaoran1 <162036361+Niuhaoran1@users.noreply.github.com>
2024-11-19 23:08:44 +0800

    第二部分第一个类

--- src/GeneralPractitioner.java
+++ src/GeneralPractitioner.java
@@ -1,2 +1,17 @@
-  
+public class GeneralPractitioner extends HealthProfessional {
+    private String specialty; 
+    public GeneralPractitioner() {
+        super();
+    }
+    public GeneralPractitioner(int ID, String name, String qualification, String specialty) {
+        super(ID, name, qualification);
+        this.specialty = specialty;
+    }
+    @Override
+    public void printDetails() {
+        super.printDetails();
+        System.out.println("Type: General Practitioner");
+        System.out.println("Specialty: " + specialty);
+    }
+}     
 
