a8e3a4e98674d6bf61882b75dbf6068acf622960
Niuhaoran1 <162036361+Niuhaoran1@users.noreply.github.com>
2024-11-19 23:43:28 +0800

    Appointment

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,1 +1,30 @@
+public class Appointment {
+    private String patientName;
+    String patientPhoneNumber;
 
+    private String preferredTimeSlot;
+
+    private HealthProfessional selectedDoctor;
+
+    public Appointment() {
+    }
+
+    public Appointment(String patientName, String patientPhoneNumber, String preferredTimeSlot, HealthProfessional selectedDoctor) {
+        this.patientName = patientName;
+        this.patientPhoneNumber = patientPhoneNumber;
+        this.preferredTimeSlot = preferredTimeSlot;
+        this.selectedDoctor = selectedDoctor;
+    }
+
+    public void printAppointmentDetails() {
+        System.out.println("Patient Name: " + patientName);
+        System.out.println("Patient Phone Number: " + patientPhoneNumber);
+        System.out.println("Preferred Time Slot: " + preferredTimeSlot);
+        System.out.println("Selected Doctor: ");
+        if (selectedDoctor != null) {
+            selectedDoctor.printDetails();
+        } else {
+            System.out.println("No doctor selected.");
+        }
+    }
+}
